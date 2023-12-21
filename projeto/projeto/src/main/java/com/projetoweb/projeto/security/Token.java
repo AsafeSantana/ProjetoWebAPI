9adeb1f6ba6ea8dc104633fdbaa3ebc089d94a48
Asafe <audioasafe@gmail.com>
2023-12-20 21:33:23 -0300

    token pt2

---
+++ projeto/projeto/src/main/java/com/projetoweb/projeto/security/Token.java
@@ -0,0 +1,16 @@
+package com.projetoweb.projeto.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Token {
+    private String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    
+}
---
+++ projeto/projeto/src/main/java/com/projetoweb/projeto/security/TokenUtil.java
@@ -0,0 +1,73 @@
+package com.projetoweb.projeto.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import com.projetoweb.projeto.model.Usuario;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.lang.Collections;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecureDigestAlgorithm;
+
+public class TokenUtil {
+
+    private static final String HEADER = "Authorization";
+    private static final String PREFIX = "Bearer";
+    private static final long EXPIRATION = 12 * 60 * 1000;
+    private static final String SECRET_KEY = "abcdf0123456789abcdf0123456789";
+    private static final String EMISSOR = "DevNice";
+
+    private static String createToken(Usuario usuario) {
+        Key secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+        String token = Jwts.builder()
+                .subject(usuario.getNome())
+                .issuer(EMISSOR)
+                .expiration(new Date(System.currentTimeMillis()
+                        + EXPIRATION))
+                .signWith(secretKey)
+                .compact();
+
+        return PREFIX + " " + token;
+    }
+
+    private static boolean isExpirationValid(Date expiration) {
+        return expiration.after(new Date(System.currentTimeMillis()));
+    }
+
+    private static boolean isEmissorValid(String emissor) {
+        return emissor.equals(EMISSOR);
+    }
+
+    private static boolean isSubjectValid(String username) {
+        return username != null && username.length() > 0;
+    }
+
+    public static Authentication validate(HttpServletRequest request) {
+        String token = request.getHeader(HEADER);
+        token = token.replace(PREFIX, "");
+
+        Jws<Claims> jwsClaims = Jwts.parser()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build()
+                .parseClaimsJws(token);
+
+        String username = jwsClaims.getPayload().getSubject();
+        String issuer = jwsClaims.getPayload().getIssuer();
+        Date expira = jwsClaims.getPayload().getExpiration();
+
+        if (isSubjectValid(username) && isEmissorValid(issuer) && isExpirationValid(expira)) {
+            return new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+        }
+
+        return null;
+    }
+}
